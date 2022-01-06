f600191b24ce393486f0a348f91df8095dbdd002
564155611 <564155611@qq.com>
2022-01-06 20:43:55 +0800

    增加模块xa-mycat

---
+++ xa-atomikos/src/main/java/com/fanx/xa/atomikos/XaDemoApplication.java
@@ -0,0 +1,11 @@
+package com.fanx.xa.atomikos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XaDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XaDemoApplication.class);
+    }
+}
---
+++ xa-atomikos/src/main/java/com/fanx/xa/atomikos/config/XaTransactionManager.java
@@ -0,0 +1,20 @@
+package com.fanx.xa.atomikos.config;
+
+import com.atomikos.icatch.jta.UserTransactionImp;
+import com.atomikos.icatch.jta.UserTransactionManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.jta.JtaTransactionManager;
+
+import javax.transaction.UserTransaction;
+
+@Configuration
+public class XaTransactionManager {
+    @Bean("xaTransaction")
+    JtaTransactionManager jtaTransactionManager(){
+        UserTransaction userTransaction = new UserTransactionImp();
+        UserTransactionManager userTransactionManager = new UserTransactionManager();
+        JtaTransactionManager jtaTransactionManager = new JtaTransactionManager(userTransaction,userTransactionManager);
+        return jtaTransactionManager;
+    }
+}
